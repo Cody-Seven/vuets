82341fe991e493bf8051cb975245d8cb97648e35
Cody <244635969@qq.com>
2018-11-26 17:07:57 +0800

    feat: mock

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,8 +1,8 @@
 import Mock from 'mockjs'
-import articles from 'articles'
+import articles from './articles'
 
 // articles
-Mock.mock('./articles', 'get', () => {
+Mock.mock('/articles', 'get', () => {
   return {
     code: 200,
     result: articles
